16358850792b79cb2386106c12bb5f530062aa7d
Ashish <ashishkhobragade45@gmail.com>
2018-06-11 18:23:56 +0530

    save in txt remaining

--- app/src/main/java/asia/getl/dmc/Report.java
+++ app/src/main/java/asia/getl/dmc/Report.java
@@ -1,50 +1,46 @@
 package asia.getl.dmc;
 
 
-import android.graphics.Color;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
-import android.widget.ListView;
 import android.widget.Toast;
 
 
-import com.github.mikephil.charting.charts.BarChart;
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.data.BarData;
-import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.jjoe64.graphview.GraphView;
-import com.jjoe64.graphview.ValueDependentColor;
-import com.jjoe64.graphview.helper.GraphViewXML;
-import com.jjoe64.graphview.series.BarGraphSeries;
-import com.jjoe64.graphview.series.DataPoint;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
+import asia.getl.dmc.model.Driver_history;
 import asia.getl.dmc.model.Items;
 import asia.getl.dmc.model.MyAdapter;
-import asia.getl.dmc.model.MyValueFormatter;
-import com.github.mikephil.charting.data.Entry;
+
+import org.json.JSONObject;
 
 
 public class Report extends AppCompatActivity {
@@ -52,10 +48,10 @@ public class Report extends AppCompatActivity {
     String company,zone,dmc;
     MaterialEditText edt_date;
     FirebaseDatabase database;
-    DatabaseReference report;
+    DatabaseReference report,ref_red,ref_yellow,ref_green,ref_gps;
     String dateenter;
     Button btn_report;
-
+    FloatingActionButton fab_download;
     int i;
     String day;
     String redcount;
@@ -67,8 +63,10 @@ public class Report extends AppCompatActivity {
     private RecyclerView recyclerView;
 
     private List<Items> albumList;
-
-
+    FileWriter fileWriter;
+    Driver_history driver_history;
+    List<Driver_history> list;
+    Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -99,31 +97,6 @@ public class Report extends AppCompatActivity {
 
         albumList = new ArrayList<>();
 
-        //test
-        Items items = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items1 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items2 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items3 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items4 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items5 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items6 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items7 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items8 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items9 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items10 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
-        Items items11 = new Items("31","30000","30000","30000","30000");
-        albumList.add(items);
 
         adapter = new MyAdapter(albumList);
         RecyclerView.LayoutManager mLayoutManager = new GridLayoutManager(this, 5);
@@ -136,31 +109,140 @@ public class Report extends AppCompatActivity {
 
         // reportmethod();
 
-       setData();
+
         //report = database.getReference(edt_date.getText().toString().trim()+"/"+company+"/"+zone+"/"+dmc+"/");
 
-    /*    btn_report.setOnClickListener(new View.OnClickListener() {
+      btn_report.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                dateenter = edt_date.getText().toString();
+               // dateenter = edt_date.getText().toString();
 
-                reportmethod();
+                setData();
 
 
 
             }
         });
-*/
+
+      fab_download.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+
+          // new Mytask().doInBackground();
+
+             DownloadFiles();
+          }
+      });
+
 
     }
 
-    private void setData() {
-        report = database.getReference("daily_record_db"+"/"+"Jun2018"+"/"+company+"/"+zone+"/"+dmc);
+    private void DownloadFiles() {
+        report = database.getReference("daily_record_db"+"/"+edt_date.getText().toString()+"/"+company+"/"+zone+"/"+dmc);
+        File file = new File(this.getFilesDir(),"/fileName.csv");
+
+        String comma = ",";
+        String newline = "\n";
+        String File_header = "DL Number,Name,Plant,Status,Share Topics,TBT topic,DDI topic,Trainer Name";
+         driver_history = new Driver_history();
+         list = new ArrayList<Driver_history>();
 
         report.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+
+                /*String dlno,name,plant,status,topics,tbt,ddi,trainer;
+                dlno = driver_history.getHistory_cdlno();
+                name = driver_history.getHistory_cname();
+                plant = driver_history.getHistory_cplant();
+                status = driver_history.getHistory_status();
+                topics = driver_history.getHistory_sharedtopics();
+                tbt = driver_history.getHistory_tbt();
+                ddi = driver_history.getHistory_ddi();
+                trainer = driver_history.getHistory_ctrainername();
+
+                list.add(new Driver_history(dlno,name,plant,status,topics,tbt,ddi,trainer));*/
+
+
+            }
+
+
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+
+
+        });
+
+       /* try {
+
+          //  String filePath = context.getFilesDir().getPath() + "/fileName.csv";
+            fileWriter = new FileWriter(file);
+            fileWriter.append(File_header);
+            for(Driver_history d : list)
+            {
+                fileWriter.append(newline);
+                fileWriter.append(d.getHistory_cdlno());
+                Log.e("Test",d.getHistory_cdlno());
+                fileWriter.append(comma);
+                fileWriter.append(d.getHistory_cname());
+                fileWriter.append(comma);
+                fileWriter.append(d.getHistory_cplant());
+                fileWriter.append(comma);
+                fileWriter.append(d.getHistory_status());
+                fileWriter.append(comma);
+                fileWriter.append(d.getHistory_sharedtopics());
+                fileWriter.append(comma);
+                fileWriter.append(d.getHistory_tbt());
+                fileWriter.append(comma);
+                fileWriter.append(d.getHistory_ddi());
+                fileWriter.append(comma);
+                fileWriter.append(d.getHistory_ctrainername());
+
+            }
+
+            fileWriter.flush();
+            fileWriter.close();
+
+            Toast.makeText(this, "Completed", Toast.LENGTH_SHORT).show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+*/
+
+    }
+
+
+    private void setData() {
+        report = database.getReference("daily_record_db"+"/"+edt_date.getText().toString()+"/"+company+"/"+zone+"/"+dmc);
+
+
+
+        report.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+
                 day = dataSnapshot.getKey();
                 redcount = String.valueOf(dataSnapshot.child("Red").getChildrenCount());
                 yellowcount = String.valueOf(dataSnapshot.child("Yellow").getChildrenCount());
@@ -197,6 +279,8 @@ public class Report extends AppCompatActivity {
 
             }
         });
+
+
 
 
 
@@ -362,10 +446,27 @@ public class Report extends AppCompatActivity {
     private void findViewById() {
         edt_date = (MaterialEditText)findViewById(R.id.edt_date);
         btn_report = (Button)findViewById(R.id.btn_report);
+        fab_download = (FloatingActionButton)findViewById(R.id.fab_download);
+
 
 
 
     }
 
+    private class Mytask extends AsyncTask<Void,Void,Void>{
 
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            DownloadFiles();
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Toast.makeText(Report.this, "Completed", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
+
---
+++ app/src/main/java/asia/getl/dmc/model/Driver_history.java
@@ -0,0 +1,230 @@
+package asia.getl.dmc.model;
+
+public class Driver_history {
+
+   private String history_cdlno,history_caadhar,history_cname,history_cepassport,history_cdlvalidity,history_cplant,history_ctransporter,history_status,history_corigin,
+            history_cdestination,history_vehical,history_gps,history_cnumbertrip,history_coverspeed,history_charshbreak,history_caccelation,history_cdriving,
+            history_rating,history_sharedtopics,history_tbt,history_ddi,history_ctrainername;
+
+    public Driver_history() {
+    }
+
+   /* public Driver_history(String history_cdlno, String history_cname, String history_cplant, String history_status, String history_sharedtopics,
+                          String history_ctrainername) {
+
+
+    }*/
+
+    public Driver_history(String history_cdlno, String history_cname, String history_cplant, String history_status, String history_sharedtopics, String history_tbt, String history_ddi, String history_ctrainername) {
+        this.history_cdlno = history_cdlno;
+        this.history_cname = history_cname;
+        this.history_cplant = history_cplant;
+        this.history_status = history_status;
+        this.history_sharedtopics = history_sharedtopics;
+        this.history_tbt = history_tbt;
+        this.history_ddi = history_ddi;
+        this.history_ctrainername = history_ctrainername;
+    }
+
+
+    public Driver_history(String history_cdlno, String history_caadhar, String history_cname, String history_cepassport, String history_cdlvalidity, String history_cplant, String history_ctransporter, String history_status, String history_corigin, String history_cdestination, String history_vehical, String history_gps, String history_cnumbertrip, String history_coverspeed, String history_charshbreak, String history_caccelation, String history_cdriving, String history_rating, String history_sharedtopics, String history_tbt, String history_ddi, String history_ctrainername) {
+        this.history_cdlno = history_cdlno;
+        this.history_caadhar = history_caadhar;
+        this.history_cname = history_cname;
+        this.history_cepassport = history_cepassport;
+        this.history_cdlvalidity = history_cdlvalidity;
+        this.history_cplant = history_cplant;
+        this.history_ctransporter = history_ctransporter;
+        this.history_status = history_status;
+        this.history_corigin = history_corigin;
+        this.history_cdestination = history_cdestination;
+        this.history_vehical = history_vehical;
+        this.history_gps = history_gps;
+        this.history_cnumbertrip = history_cnumbertrip;
+        this.history_coverspeed = history_coverspeed;
+        this.history_charshbreak = history_charshbreak;
+        this.history_caccelation = history_caccelation;
+        this.history_cdriving = history_cdriving;
+        this.history_rating = history_rating;
+        this.history_sharedtopics = history_sharedtopics;
+        this.history_tbt = history_tbt;
+        this.history_ddi = history_ddi;
+        this.history_ctrainername = history_ctrainername;
+    }
+
+    public String getHistory_tbt() {
+        return history_tbt;
+    }
+
+    public void setHistory_tbt(String history_tbt) {
+        this.history_tbt = history_tbt;
+    }
+
+    public String getHistory_ddi() {
+        return history_ddi;
+    }
+
+    public void setHistory_ddi(String history_ddi) {
+        this.history_ddi = history_ddi;
+    }
+
+    public String getHistory_cdlno() {
+        return history_cdlno;
+    }
+
+    public void setHistory_cdlno(String history_cdlno) {
+        this.history_cdlno = history_cdlno;
+    }
+
+    public String getHistory_caadhar() {
+        return history_caadhar;
+    }
+
+    public void setHistory_caadhar(String history_caadhar) {
+        this.history_caadhar = history_caadhar;
+    }
+
+    public String getHistory_cname() {
+        return history_cname;
+    }
+
+    public void setHistory_cname(String history_cname) {
+        this.history_cname = history_cname;
+    }
+
+    public String getHistory_cepassport() {
+        return history_cepassport;
+    }
+
+    public void setHistory_cepassport(String history_cepassport) {
+        this.history_cepassport = history_cepassport;
+    }
+
+    public String getHistory_cdlvalidity() {
+        return history_cdlvalidity;
+    }
+
+    public void setHistory_cdlvalidity(String history_cdlvalidity) {
+        this.history_cdlvalidity = history_cdlvalidity;
+    }
+
+    public String getHistory_cplant() {
+        return history_cplant;
+    }
+
+    public void setHistory_cplant(String history_cplant) {
+        this.history_cplant = history_cplant;
+    }
+
+    public String getHistory_ctransporter() {
+        return history_ctransporter;
+    }
+
+    public void setHistory_ctransporter(String history_ctransporter) {
+        this.history_ctransporter = history_ctransporter;
+    }
+
+    public String getHistory_status() {
+        return history_status;
+    }
+
+    public void setHistory_status(String history_status) {
+        this.history_status = history_status;
+    }
+
+    public String getHistory_corigin() {
+        return history_corigin;
+    }
+
+    public void setHistory_corigin(String history_corigin) {
+        this.history_corigin = history_corigin;
+    }
+
+    public String getHistory_cdestination() {
+        return history_cdestination;
+    }
+
+    public void setHistory_cdestination(String history_cdestination) {
+        this.history_cdestination = history_cdestination;
+    }
+
+    public String getHistory_vehical() {
+        return history_vehical;
+    }
+
+    public void setHistory_vehical(String history_vehical) {
+        this.history_vehical = history_vehical;
+    }
+
+    public String getHistory_gps() {
+        return history_gps;
+    }
+
+    public void setHistory_gps(String history_gps) {
+        this.history_gps = history_gps;
+    }
+
+    public String getHistory_cnumbertrip() {
+        return history_cnumbertrip;
+    }
+
+    public void setHistory_cnumbertrip(String history_cnumbertrip) {
+        this.history_cnumbertrip = history_cnumbertrip;
+    }
+
+    public String getHistory_coverspeed() {
+        return history_coverspeed;
+    }
+
+    public void setHistory_coverspeed(String history_coverspeed) {
+        this.history_coverspeed = history_coverspeed;
+    }
+
+    public String getHistory_charshbreak() {
+        return history_charshbreak;
+    }
+
+    public void setHistory_charshbreak(String history_charshbreak) {
+        this.history_charshbreak = history_charshbreak;
+    }
+
+    public String getHistory_caccelation() {
+        return history_caccelation;
+    }
+
+    public void setHistory_caccelation(String history_caccelation) {
+        this.history_caccelation = history_caccelation;
+    }
+
+    public String getHistory_cdriving() {
+        return history_cdriving;
+    }
+
+    public void setHistory_cdriving(String history_cdriving) {
+        this.history_cdriving = history_cdriving;
+    }
+
+    public String getHistory_rating() {
+        return history_rating;
+    }
+
+    public void setHistory_rating(String history_rating) {
+        this.history_rating = history_rating;
+    }
+
+    public String getHistory_sharedtopics() {
+        return history_sharedtopics;
+    }
+
+    public void setHistory_sharedtopics(String history_sharedtopics) {
+        this.history_sharedtopics = history_sharedtopics;
+    }
+
+    public String getHistory_ctrainername() {
+        return history_ctrainername;
+    }
+
+    public void setHistory_ctrainername(String history_ctrainername) {
+        this.history_ctrainername = history_ctrainername;
+    }
+}
