96c0ad8c8088b99d36c7d7024e563ea5c548796b
Ashish <ashishkhobragade45@gmail.com>
2018-06-12 18:09:45 +0530

    working on header

--- app/src/main/java/asia/getl/dmc/Report.java
+++ app/src/main/java/asia/getl/dmc/Report.java
@@ -1,8 +1,11 @@
 package asia.getl.dmc;
 
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.os.AsyncTask;
+import android.os.Environment;
 import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -16,6 +19,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 
@@ -29,9 +33,17 @@ import com.google.firebase.database.ValueEventListener;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Writer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -39,7 +51,7 @@ import java.util.Map;
 import asia.getl.dmc.model.Driver_history;
 import asia.getl.dmc.model.Items;
 import asia.getl.dmc.model.MyAdapter;
-
+import java.io.PrintWriter;
 import org.json.JSONObject;
 
 
@@ -48,7 +60,7 @@ public class Report extends AppCompatActivity {
     String company,zone,dmc;
     MaterialEditText edt_date;
     FirebaseDatabase database;
-    DatabaseReference report,ref_red,ref_yellow,ref_green,ref_gps;
+    DatabaseReference report,report1,ref_red,ref_yellow,ref_green,ref_gps;
     String dateenter;
     Button btn_report;
     FloatingActionButton fab_download;
@@ -59,14 +71,19 @@ public class Report extends AppCompatActivity {
     String greencount;
     String gpscount;
 
+    String str_red,red1,red2;
+
     MyAdapter adapter;
     private RecyclerView recyclerView;
-
+    String history_cdlno,history_cname,history_cplant, history_status,
+    history_sharedtopics, history_tbt, history_ddi,history_ctrainername;
     private List<Items> albumList;
     FileWriter fileWriter;
     Driver_history driver_history;
-    List<Driver_history> list;
-    Context context;
+    BufferedWriter fos = null;
+
+
+    ArrayList<Driver_history> listfile;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -129,108 +146,155 @@ public class Report extends AppCompatActivity {
           @Override
           public void onClick(View v) {
 
-          // new Mytask().doInBackground();
+         // new Mytask().execute();
 
-             DownloadFiles();
+           //DownloadFiles();
+              exportfile();
           }
       });
 
 
     }
 
-    private void DownloadFiles() {
-        report = database.getReference("daily_record_db"+"/"+edt_date.getText().toString()+"/"+company+"/"+zone+"/"+dmc);
-        File file = new File(this.getFilesDir(),"/fileName.csv");
 
-        String comma = ",";
-        String newline = "\n";
-        String File_header = "DL Number,Name,Plant,Status,Share Topics,TBT topic,DDI topic,Trainer Name";
-         driver_history = new Driver_history();
-         list = new ArrayList<Driver_history>();
-
-        report.addChildEventListener(new ChildEventListener() {
-            @Override
-            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-
-                /*String dlno,name,plant,status,topics,tbt,ddi,trainer;
-                dlno = driver_history.getHistory_cdlno();
-                name = driver_history.getHistory_cname();
-                plant = driver_history.getHistory_cplant();
-                status = driver_history.getHistory_status();
-                topics = driver_history.getHistory_sharedtopics();
-                tbt = driver_history.getHistory_tbt();
-                ddi = driver_history.getHistory_ddi();
-                trainer = driver_history.getHistory_ctrainername();
-
-                list.add(new Driver_history(dlno,name,plant,status,topics,tbt,ddi,trainer));*/
-
-
-            }
+    private void exportfile() {
 
 
 
-            @Override
-            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
-
-            }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-
-            }
-
-
-        });
-
-       /* try {
-
-          //  String filePath = context.getFilesDir().getPath() + "/fileName.csv";
-            fileWriter = new FileWriter(file);
-            fileWriter.append(File_header);
-            for(Driver_history d : list)
-            {
-                fileWriter.append(newline);
-                fileWriter.append(d.getHistory_cdlno());
-                Log.e("Test",d.getHistory_cdlno());
-                fileWriter.append(comma);
-                fileWriter.append(d.getHistory_cname());
-                fileWriter.append(comma);
-                fileWriter.append(d.getHistory_cplant());
-                fileWriter.append(comma);
-                fileWriter.append(d.getHistory_status());
-                fileWriter.append(comma);
-                fileWriter.append(d.getHistory_sharedtopics());
-                fileWriter.append(comma);
-                fileWriter.append(d.getHistory_tbt());
-                fileWriter.append(comma);
-                fileWriter.append(d.getHistory_ddi());
-                fileWriter.append(comma);
-                fileWriter.append(d.getHistory_ctrainername());
-
-            }
-
-            fileWriter.flush();
-            fileWriter.close();
-
-            Toast.makeText(this, "Completed", Toast.LENGTH_SHORT).show();
-
+        String dateyear = edt_date.getText().toString();
+        final String path = "daily_record_db"+"/"+edt_date.getText().toString()+"/"+company+"/"+zone+"/"+dmc;
+        //report1 = database.getReference("daily_record_db"+"/"+edt_date.getText().toString()+"/"+company+"/"+zone+"/"+dmc);
+        try {
+            fos = new BufferedWriter(new FileWriter(Environment.getExternalStorageDirectory().getAbsolutePath() +"/"+"TestFinal5.txt",true));
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+            try {
+                fos.write("DL number"+";"+"Driver Name"+";"+"Plant"+";"+"Status"+";"+"Topic"+";"+"TBT topic"+";"+"DDI topic"
+                        +";"+"Trainer Name"+"\n");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+
+        for( i =0;i<30;i++)
+        {
+
+
+            report1 = database.getReference(path+"/"+i);
+            report1.child("Red").addChildEventListener(new ChildEventListener() {
+                @Override
+                public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                   /* String test = dataSnapshot.getKey();
+                    Log.d("test",test);
+                    Driver_history driver_history = dataSnapshot.getValue(Driver_history.class);
+                    String dlno = driver_history.getHistory_cplant();
+                    Log.d("DL NO",dlno);*/
+                    Driver_history driver_history = dataSnapshot.getValue(Driver_history.class);
+                   String rdlno,name,plant,status,topic,tbt,ddi,trainer;
+                   rdlno = driver_history.getHistory_cdlno();
+                   name = driver_history.getHistory_cname();
+                   plant = driver_history.getHistory_cplant();
+                   status = driver_history.getHistory_status();
+                   topic = driver_history.getHistory_sharedtopics();
+                   tbt = driver_history.getHistory_tbt();
+                   ddi = driver_history.getHistory_ddi();
+                   trainer = driver_history.getHistory_ctrainername();
+
+                   String test =rdlno+";"+name+";"+plant+";"+status+";"+topic+";"+tbt+";"+ddi+";"+trainer+"\n";
+                   Log.d("Test123",test);
+
+
+                   if(!test.isEmpty()) {
+                      //listfile.add();
+                       try {
+                           fos = new BufferedWriter(new FileWriter(Environment.getExternalStorageDirectory().getAbsolutePath() +"/"+"TestFinal5.txt",true));
+                       } catch (IOException e) {
+                           e.printStackTrace();
+                       }
+
+                       try {
+                           fos.write(test);
+                       } catch (IOException e) {
+                           e.printStackTrace();
+                       }
+                       try {
+                           fos.close();
+                       } catch (IOException e) {
+                           e.printStackTrace();
+                       }
+
+
+
+                       // listfile.add(rdlno+","+name+","+plant+","+status+","+topic+","+tbt+","+ddi+","+trainer+"\n");
+                   }
+                }
+
+
+                @Override
+                public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+
+                }
+
+                @Override
+                public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                }
+            });
+
+
+
+
+
+/*
+
+            BufferedWriter fos = null;
+                try {
+                    fos = new BufferedWriter(new FileWriter(Environment.getExternalStorageDirectory().getAbsolutePath() +"/"+"File1.txt"));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+                try {
+                    fos.write(str);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                try {
+                    fos.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 */
 
-    }
+        }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
 
 
     private void setData() {
@@ -453,20 +517,6 @@ public class Report extends AppCompatActivity {
 
     }
 
-    private class Mytask extends AsyncTask<Void,Void,Void>{
 
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            DownloadFiles();
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            Toast.makeText(Report.this, "Completed", Toast.LENGTH_SHORT).show();
-        }
-    }
 }
 
